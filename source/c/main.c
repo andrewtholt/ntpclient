03ca765407ceeea7c2af04c814394f6eef4a664f
Andrew Holt <andrewtholt60@gmail.com>
2021-07-09 13:18:15 +0100

    Output more data, and take time source on command line

--- source/c/main.c
+++ source/c/main.c
@@ -39,122 +39,131 @@ void error( char* msg )
     exit( 0 ); // Quit the process.
 }
 
-int main( int argc, char* argv[ ] )
-{
-  int sockfd, n; // Socket file descriptor and the n return result from writing/reading from the socket.
+int main( int argc, char* argv[ ] ) {
 
-  int portno = 123; // NTP UDP port number.
+    int sockfd, n; // Socket file descriptor and the n return result from writing/reading from the socket.
 
-  char* host_name = "us.pool.ntp.org"; // NTP server host-name.
+    int portno = 123; // NTP UDP port number.
 
-  // Structure that defines the 48 byte NTP packet protocol.
 
-  typedef struct
-  {
+    char* host_name;
+    if (argc == 1) {
+        host_name = "us.pool.ntp.org"; // NTP server host-name.
+    } else {
+        host_name = argv[1];
+    }
 
-    uint8_t li_vn_mode;      // Eight bits. li, vn, and mode.
-                             // li.   Two bits.   Leap indicator.
-                             // vn.   Three bits. Version number of the protocol.
-                             // mode. Three bits. Client will pick mode 3 for client.
+    printf("Getting time from %s\n", host_name);
 
-    uint8_t stratum;         // Eight bits. Stratum level of the local clock.
-    uint8_t poll;            // Eight bits. Maximum interval between successive messages.
-    uint8_t precision;       // Eight bits. Precision of the local clock.
+    // Structure that defines the 48 byte NTP packet protocol.
 
-    uint32_t rootDelay;      // 32 bits. Total round trip delay time.
-    uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
-    uint32_t refId;          // 32 bits. Reference clock identifier.
+    typedef struct
+    {
 
-    uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
-    uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
+        uint8_t li_vn_mode;      // Eight bits. li, vn, and mode.
+        // li.   Two bits.   Leap indicator.
+        // vn.   Three bits. Version number of the protocol.
+        // mode. Three bits. Client will pick mode 3 for client.
 
-    uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
-    uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
+        uint8_t stratum;         // Eight bits. Stratum level of the local clock.
+        uint8_t poll;            // Eight bits. Maximum interval between successive messages.
+        uint8_t precision;       // Eight bits. Precision of the local clock.
 
-    uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
-    uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
+        uint32_t rootDelay;      // 32 bits. Total round trip delay time.
+        uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
+        uint32_t refId;          // 32 bits. Reference clock identifier.
 
-    uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
-    uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
+        uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
+        uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
 
-  } ntp_packet;              // Total: 384 bits or 48 bytes.
+        uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
+        uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
 
-  // Create and zero out the packet. All 48 bytes worth.
+        uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
+        uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
 
-  ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
+        uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
 
-  memset( &packet, 0, sizeof( ntp_packet ) );
+    } ntp_packet;              // Total: 384 bits or 48 bytes.
 
-  // Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
+    // Create and zero out the packet. All 48 bytes worth.
 
-  *( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
+    ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-  // Create a UDP socket, convert the host-name to an IP address, set the port number,
-  // connect to the server, send the packet, and then read in the return packet.
+    memset( &packet, 0, sizeof( ntp_packet ) );
 
-  struct sockaddr_in serv_addr; // Server address data structure.
-  struct hostent *server;      // Server data structure.
+    // Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
 
-  sockfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
+    *( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
 
-  if ( sockfd < 0 )
-    error( "ERROR opening socket" );
+    // Create a UDP socket, convert the host-name to an IP address, set the port number,
+    // connect to the server, send the packet, and then read in the return packet.
 
-  server = gethostbyname( host_name ); // Convert URL to IP.
+    struct sockaddr_in serv_addr; // Server address data structure.
+    struct hostent *server;      // Server data structure.
 
-  if ( server == NULL )
-    error( "ERROR, no such host" );
+    sockfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
 
-  // Zero out the server address structure.
+    if ( sockfd < 0 )
+        error( "ERROR opening socket" );
 
-  bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
+    server = gethostbyname( host_name ); // Convert URL to IP.
 
-  serv_addr.sin_family = AF_INET;
+    if ( server == NULL )
+        error( "ERROR, no such host" );
 
-  // Copy the server's IP address to the server address structure.
+    // Zero out the server address structure.
 
-  bcopy( ( char* )server->h_addr, ( char* ) &serv_addr.sin_addr.s_addr, server->h_length );
+    bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
 
-  // Convert the port number integer to network big-endian style and save it to the server address structure.
+    serv_addr.sin_family = AF_INET;
 
-  serv_addr.sin_port = htons( portno );
+    // Copy the server's IP address to the server address structure.
 
-  // Call up the server using its IP address and port number.
+    bcopy( ( char* )server->h_addr, ( char* ) &serv_addr.sin_addr.s_addr, server->h_length );
 
-  if ( connect( sockfd, ( struct sockaddr * ) &serv_addr, sizeof( serv_addr) ) < 0 )
-    error( "ERROR connecting" );
+    // Convert the port number integer to network big-endian style and save it to the server address structure.
 
-  // Send it the NTP packet it wants. If n == -1, it failed.
+    serv_addr.sin_port = htons( portno );
 
-  n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+    // Call up the server using its IP address and port number.
 
-  if ( n < 0 )
-    error( "ERROR writing to socket" );
+    if ( connect( sockfd, ( struct sockaddr * ) &serv_addr, sizeof( serv_addr) ) < 0 )
+        error( "ERROR connecting" );
 
-  // Wait and receive the packet back from the server. If n == -1, it failed.
+    // Send it the NTP packet it wants. If n == -1, it failed.
 
-  n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+    n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
 
-  if ( n < 0 )
-    error( "ERROR reading from socket" );
+    if ( n < 0 )
+        error( "ERROR writing to socket" );
 
-  // These two fields contain the time-stamp seconds as the packet left the NTP server.
-  // The number of seconds correspond to the seconds passed since 1900.
-  // ntohl() converts the bit/byte order from the network's to host's "endianness".
+    // Wait and receive the packet back from the server. If n == -1, it failed.
 
-  packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
-  packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
+    n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
 
-  // Extract the 32 bits that represent the time-stamp seconds (since NTP epoch) from when the packet left the server.
-  // Subtract 70 years worth of seconds from the seconds since 1900.
-  // This leaves the seconds since the UNIX epoch of 1970.
-  // (1900)------------------(1970)**************************************(Time Packet Left the Server)
+    if ( n < 0 )
+        error( "ERROR reading from socket" );
 
-  time_t txTm = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
+    // These two fields contain the time-stamp seconds as the packet left the NTP server.
+    // The number of seconds correspond to the seconds passed since 1900.
+    // ntohl() converts the bit/byte order from the network's to host's "endianness".
 
-  // Print the time we got from the server, accounting for local timezone and conversion from UTC time.
+    packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
+    packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
 
-  printf( "Time: %s", ctime( ( const time_t* ) &txTm ) );
+    // Extract the 32 bits that represent the time-stamp seconds (since NTP epoch) from when the packet left the server.
+    // Subtract 70 years worth of seconds from the seconds since 1900.
+    // This leaves the seconds since the UNIX epoch of 1970.
+    // (1900)------------------(1970)**************************************(Time Packet Left the Server)
 
-  return 0;
+    time_t txTm = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
+
+    // Print the time we got from the server, accounting for local timezone and conversion from UTC time.
+
+    printf( "Time: %s", ctime( ( const time_t* ) &txTm ) );
+    printf("Ref=%ud\n", packet.refTm_s);
+
+    return 0;
 }
